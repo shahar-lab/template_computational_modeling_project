358ee9c8f79ecfdc216cb06db9158442467b1e5b
NitzanShahar <68427888+NitzanShahar@users.noreply.github.com>
2022-06-01 23:31:18 +0300

    minor change - extracting draws
    updating the way we call the parameters from the fitted stan objects (e.g., rl_fit).
    This makes it a bit easier to examine the parameters after sampling.

--- analysis_modeling/examine_parameters.R
+++ analysis_modeling/examine_parameters.R
@@ -9,29 +9,35 @@ source('./functions/my_starter.R')
 library(ggplot2)
 library(ggpubr)
 library(bayestestR)
+library(stringr)
 
 #population level parameters
-load(paste0(data_path,'/simulate_population_parameters.Rdata'))
-load(paste0(data_path,'/modelfit_based_on_artificial_data.rdata'))
+load(paste0(path$data,'/simulate_population_parameters.Rdata'))
+load(paste0(path$data,'/modelfit_based_on_artificial_data.rdata'))
+rl_fit=readRDS(paste0(path$data,'/modelfit_based_on_empirical_data.rds'))
 
-my_posteriorplot(x       = plogis(pars$population_locations[,1]),
+my_posteriorplot(x       = plogis(rl_fit$draws(variables ='population_locations[1]',
+                                               format='draws_matrix')),
                      myxlim  = c(0,1),
-                     my_vline= plogis(population_parameters[[3]][1]), 
+                     my_vline= plogis(population_parameters[[2]][1]), 
                      myxlab  = expression(alpha['location']),
                      mycolor = "pink")
 
 
-my_posteriorplot(x       = pars$population_locations[,2],
+my_posteriorplot(x       = rl_fit$draws(variables ='population_locations[2]',
+                                        format='draws_matrix'),
                      myxlim  = c(0.5,5),
-                     my_vline= population_parameters[[3]][2], 
+                     my_vline= population_parameters[[2]][2], 
                      myxlab  = expression(beta['location']),
                      mycolor = "pink")
 
 
 #-------------------------------------------------------------------------------------------------------------
 # individual level parameters
-load(paste0(data_path,'/simulate_individual_parameters.Rdata'))
+load(paste0(path$data,'/simulate_individual_parameters.Rdata'))
+rl_fit=readRDS(paste0(path$data,'/modelfit_based_on_empirical_data.rds'))
 
-my_xyplot(individual_parameters[,'alpha_chosen'],apply(pars$alpha_ch, 2, mean),'true','recovered','navy')
-my_xyplot(individual_parameters[,'beta'],        apply(pars$beta, 2, mean),'true','recovered','navy')
+
+my_xyplot(individual_parameters[,'alpha'],apply(rl_fit$draws(variables ='alpha',format='draws_matrix'), 2, mean),'true','recovered','navy')
+my_xyplot(individual_parameters[,'beta'], apply(rl_fit$draws(variables ='beta' ,format='draws_matrix'), 2, mean),'true','recovered','navy')
 
--- analysis_modeling/modelfit_mcmc.R
+++ analysis_modeling/modelfit_mcmc.R
@@ -3,30 +3,27 @@ rm(list=ls())
 source('./functions/my_packages.R')
 source('./functions/my_starter.R')
 
+
 #--------------------------------------------------------------------------------------------------------
 
 # load data
 load(paste0(path$data,'/simulate_standata_based_on_artificial_parameters.rdata'))
 load(paste0(path$data,'/modelfit_compile.rdata'))
 
-{
-  start_time <- Sys.time()
-  
   rl_fit<- my_compiledmodel$sample(
     data = data_for_stan, 
     iter_sampling = 500,
-    iter_warmup = 500,
-    chains = 2)  
+    iter_warmup = 200,
+    chains           = 2,
+    parallel_chains  = 2)  
   
-  end_time <- Sys.time()
-  end_time-start_time
-}
 
 #save
 saveRDS(rl_fit, paste0(path$data,'/modelfit_based_on_empirical_data.rds'))
 
-pars <- as_draws_df(rl_fit$draws())
-save(pars, file=paste0(path$data,'/modelfit_based_on_artificial_data.rdata'))
+## can get the dars directly from the rl_fit object
+# pars <- as_draws_df(rl_fit$draws())
+# save(pars, file=paste0(path$data,'/modelfit_based_on_artificial_data.rdata'))
 
 
 # # without_loo
