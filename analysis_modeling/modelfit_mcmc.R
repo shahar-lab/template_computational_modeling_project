eafa7d8fd9e288ff071536912ee7c21e5eeabe44
Ido <ido.iba@gmail.com>
2022-06-02 13:48:38 +0300

    temp_modelfit_mcmc

--- analysis_modeling/modelfit_mcmc.R
+++ analysis_modeling/modelfit_mcmc.R
@@ -10,7 +10,7 @@ load(paste0(path$data,'/simulate_standata_based_on_artificial_parameters.rdata')
 load(paste0(path$data,'/modelfit_compile.rdata'))
 
 # without_loo
-fit_without_loo=run_fit(include_loo=0,data_for_stan=data_for_stan,iter_sampling=1000,iter_warmup=1000,chains=4)
+fit_without_loo=run_fit(include_loo=0,data_for_stan=data_for_stan,iter_sampling=10,iter_warmup=10,chains=2)
 # save
 saveRDS(fit_without_loo, paste0(path$data,'/modelfit_based_on_empirical_data.rds'))
 pars <- as_draws_df(fit_without_loo$draws(variables=c("alpha","beta")))  #column names are saved here with ``.
@@ -18,7 +18,7 @@ pars <- as_draws_df(fit_without_loo$draws(variables=c("alpha","beta")))  #column
 save(pars, file=paste0(path$data,'/modelfit_based_on_artificial_data.rdata'))
 
 # with_loo
-fit_with_loo=run_fit(include_loo=1,data_for_stan=data_for_stan,iter_sampling=1000,iter_warmup=1000,chains=4)
+fit_with_loo=run_fit(include_loo=1,data_for_stan=data_for_stan,iter_sampling=10,iter_warmup=10,chains=2)
 # aggregate across all blocks (the other ones are zeros)
 like=Reduce('+', fit_with_loo)
 save(like, file=paste0(path$data,'modelfit_like_per_trial_and_chain.rdata'))
