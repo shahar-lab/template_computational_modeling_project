13316a49902540c4ad8d9010247db95f6a611ed1
NitzanShahar <68427888+NitzanShahar@users.noreply.github.com>
2022-06-27 21:08:20 +0300

    Update my_starter.R

--- functions/my_starter.R
+++ functions/my_starter.R
@@ -6,6 +6,9 @@ source('./functions/my_posteriorplot.R')
 source('./functions/my_xyplot.R')
 source('./functions/remove_workingmodel.r')
 source('./functions/run_fit.r')
+source('./functions/generate_individual_parameters.r')
+source('./functions/plot_artifical_parameters.r')
+
 path=set_workingmodel()
 
 
