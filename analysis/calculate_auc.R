1209d5cb63ca8811f194077e1c2cd21cb799daeb
idoba2030 <ido.iba@gmail.com>
2023-03-09 20:47:52 +0200

    calculate_auc

---
+++ analysis/calculate_auc.R
@@ -0,0 +1,24 @@
+#load fit
+fit=readRDS(paste0(path$data,'/modelfit_empirical.rds'))
+p_ch_action=fit$draws(variables ='p_ch_action',format='draws_matrix')
+
+#remove missing trials
+p_ch_action[p_ch_action==0]=NA
+p_ch_action = p_ch_action[, !colSums(is.na(p_ch_action))]
+
+#calculate roc
+#print example
+plot(roc(df$selected_offer,p_ch_action[,1]))
+auc(roc)
+#iterate on all samples
+Nsamples=ncol(p_ch_action)
+auc_list=list()
+for (sample in 1:Nsamples){
+  roc=roc(df$selected_offer,p_ch_action[,sample])
+  auc=auc(roc)
+  auc_list=append(auc_list,auc)
+}
+posterior_auc=unlist(auc_list)
+hist(posterior_auc)
+
+save(posterior_auc,file=paste0(path$data,'/auc.Rdata'))
