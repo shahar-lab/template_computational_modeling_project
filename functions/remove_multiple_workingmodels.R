92a485877ec492d0b546f1ac314f9581e6c85aa2
shirelch <elchayani@mail.tau.ac.il>
2024-03-01 23:56:56 +0200

    Added comment

--- functions/remove_multiple_workingmodels.R
+++ functions/remove_multiple_workingmodels.R
@@ -1,18 +1,18 @@
 remove_multiple_workingmodels <- function() {
-  # Load the list of available models
-  load('functions/working_model.rdata')
-  
-  # Present a GUI select list for the user to choose multiple models
-  selected_models <- dlg_list(title = 'Select models to remove:',
-                              mymodels_list,
-                              multiple = TRUE)$res
-  
-  # Update mymodels_list by removing the selected models
-  mymodels_list <- mymodels_list[!(mymodels_list %in% selected_models)]
-  
-  # Print a message indicating which models have been removed
-  cat(paste(selected_models, 'were removed from the models list.\n'))
-  
-  # Save the updated mymodels_list back to the file
-  save(mymodels_list, file = 'functions/working_model.rdata')
+    # Load the list of available models
+    load('functions/working_model.rdata')
+    
+    # Present a GUI select list for the user to choose multiple models
+    selected_models <- dlg_list(title = 'Select models to remove:',
+                                mymodels_list,
+                                multiple = TRUE)$res
+    
+    # Update mymodels_list by removing the selected models
+    mymodels_list <- mymodels_list[!(mymodels_list %in% selected_models)]
+    
+    # Print a message indicating which models have been removed
+    cat(paste(selected_models, 'were removed from the models list.\n'))
+    
+    # Save the updated mymodels_list back to the file
+    save(mymodels_list, file = 'functions/working_model.rdata')
 }
