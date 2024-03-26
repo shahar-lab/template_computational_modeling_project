e2d8b679212ac1dd8d11a9af73161df6170982ac
shirelch <elchayani@mail.tau.ac.il>
2024-03-26 13:42:37 +0200

    Added different mains

--- stan_modeling/main_model_fit.R
+++ stan_modeling/main_model_fit.R
@@ -4,18 +4,8 @@ source('./functions/my_starter.R')
 
 path = set_workingmodel()
 
-cfg = list(
-  Nsubjects        = 5,
-  Nblocks          = 2,
-  Ntrials_perblock = 50,
-  Narms            = 2, #number of arms in the task
-  Nraffle          = 2, #number of arms offered for selection each trial
-  rndwlk           = read.csv('./functions/rndwlk.csv', header = F)
-)
-
-
-#####Simulate data--------------------
-generate_artificial_data(cfg = cfg)
+#####Load stan data--------------------
+data_path = set_standata_file()
 
 #####sample posterior--------------------
 
@@ -23,10 +13,10 @@ modelfit_compile(path, format = F)
 
 modelfit_mcmc(
   path,
-  data_path = paste0('data/stan_ready_data_files/artificial_standata_', path$name, '.Rdata'),
+  data_path = data_path,
   
   mymcmc = list(
-    datatype = 'artificial' ,
+    datatype = 'empirical' ,
     samples  = 100,
     warmup  = 200,
     chains  = 4,
@@ -38,16 +28,14 @@ modelfit_mcmc(
 mypars = c("population_scales[1]",
            "population_scales[2]")
 
-examine_mcmc(path, mypars, datatype = 'artificial')
+examine_mcmc(path, mypars, datatype = 'empirical')
 
-examine_population_parameters_recovery(path, datatype = 'artificial')
-
-examine_individual_parameters_recovery(path)
+examine_population_parameters_recovery(path, datatype = 'empirical')
 
 
 ####examine model
 #load parameters
-fit   = readRDS(paste0(path$data, '/modelfit_recovery.rds'))
+fit   = readRDS(paste0(path$data, '/modelfit_empirical.rds'))
 Qdiff = fit$draws(variables = 'Qdiff_external', format = 'draws_matrix')
 Qval1 = fit$draws(variables = 'Qval1_external', format = 'draws_matrix')
 Qval2 = fit$draws(variables = 'Qval2_external', format = 'draws_matrix')
@@ -55,3 +43,5 @@ Qval3 = fit$draws(variables = 'Qval3_external', format = 'draws_matrix')
 Qval4 = fit$draws(variables = 'Qval4_external', format = 'draws_matrix')
 
 PE    = fit$draws(variables = 'PE_external', format = 'draws_matrix')
+
+
